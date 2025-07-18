90cd96ea11890482209afa5d0a7068d5e7266738
sbframee <104187401+sbframee@users.noreply.github.com>
2025-07-18 19:57:05 +0530

    update

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ mongoose.connect(process.env.MONGO_URI);
 
 const templateRoutes = require('./routes/templateRoutes');
 app.use('/api/templates', templateRoutes);
-app.use('*', (req, res) => {
+// Also valid:
+app.use((req, res) => {
   res.status(404).json({ error: 'Route not found', path: req.originalUrl });
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
